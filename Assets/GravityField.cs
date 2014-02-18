d36050ec9113babf08cb4cadeb860f6277604dd4
Gulshan Singh <gulshan@umich.edu>
2014-02-18 18:31:56 -0500

    Got basic gravity mechanics working

---
+++ Assets/GravityField.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityField : MonoBehaviour {
+	public const float gravityConst = 10;
+	public const float mass = 10;
+
+	public Vector3 GetForce(Vector3 position) {
+		Vector3 r = transform.position - position;
+		float rMag = r.magnitude;
+		Vector3 rNorm = r.normalized;
+		return (gravityConst * mass/(rMag * rMag)) * rNorm;
+	}
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	public GameObject spherePrefab;
+
+	private GameObject planet;
+
+	private const float deltaTime = 0.02f;
+
+	void Start() {
+		planet = GameObject.Find("Planet");
+
+		CalculatePath();
+	}
+	
+	void CalculatePath() {
+		GravityField gf = planet.GetComponent<GravityField>();
+		Vector3 velocity = Vector3.left * 10;
+		Vector3 position = transform.position;
+		for (int i = 0; i < 100; i++) {
+			Vector3 f = gf.GetForce(position);
+
+			velocity += deltaTime * f;
+			position += deltaTime * velocity;
+
+			GameObject sphere = Instantiate(spherePrefab) as GameObject;
+			sphere.transform.position = position;
+		}
+	}
+}
